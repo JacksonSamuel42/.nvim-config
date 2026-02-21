5ee20565488c7a5dc060fea069bae11cedffb26a
Jackson Samuel <samuelteixeira044@gmail.com>
2026-02-21 03:15:24 +0100

    comment plugin update

--- lua/nconf/lazy/comment.lua
+++ lua/nconf/lazy/comment.lua
@@ -1,26 +1,27 @@
 return {
     {
         'numToStr/Comment.nvim',
-
+        dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
         config = function()
-        require('Comment').setup {
-            pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-            padding = true,
-            sticky = true,
-            ignore = nil,
-            toggler = {
-                ---Line-comment toggle keymap
-                line = 'gcc',
-                ---Block-comment toggle keymap
-                block = 'gbc',
-            },
-        }
+        require('Comment').setup({
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+                                 padding = true,
+                                 sticky = true,
+                                 toggler = {
+                                     line = 'gcc',
+                                 block = 'gbc',
+                                 },
+        })
+
+        -- KEYMAPS
+        local keymap = vim.keymap.set
+        keymap('n', '<leader>/', 'gcc', { remap = true, desc = "Toggle comment" })
+        keymap('v', '<leader>/', 'gc', { remap = true, desc = "Toggle comment" })
         end
     },
 
     {
         'JoosepAlviste/nvim-ts-context-commentstring',
-
         config = function()
         require('ts_context_commentstring').setup {
             enable_autocmd = false,
